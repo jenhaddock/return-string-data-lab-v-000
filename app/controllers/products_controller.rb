32f4701cf9cb8aefb0cc849b7ec8c341da70c934
Jennifer Haddock <jentx123@hotmail.com>
2017-12-06 02:32:20 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(product_params)
     @product.save
-    redirect_to product_path
+    redirect_to products_path
   end
 
   def edit
